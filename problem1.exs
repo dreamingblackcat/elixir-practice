6106b697f35559dcaf3944ae87a6050bf8cb8a76
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-01-12 10:45:30 +0630

    I was mistaken problem 1 to be multiples of 3 and 5, but actual was 3 or 5 , so I changed the solution to fit the correct problem

--- problem1.exs
+++ problem1.exs
@@ -7,6 +7,9 @@
 
 #    Find the sum of all the multiples of 3 or 5 below 1000.
 
+   # Answer: e1edf9d1967ca96767dcc2b2d6df69f4
+
+
 defmodule Problem1 do 
 	def is_multiple_of?(div,denom), do: rem(denom, div) == 0
 
@@ -14,15 +17,15 @@ defmodule Problem1 do
 
 	def is_multiple_of_five?(num), do: is_multiple_of?(5,num)
 
-	def is_multiple_of_three_and_five?(num), do: is_multiple_of_three?(num) and is_multiple_of_five?(num)
+	def is_multiple_of_three_or_five?(num), do: is_multiple_of_three?(num) or is_multiple_of_five?(num)
 
-	def multiples_of_three_and_five_under(num) do
+	def multiples_of_three_or_five_under(num) do
 		#use erlang stdlib function for genrating a list grown by step 5
-		:lists.seq(0,1000,5) |> Enum.filter(&is_multiple_of_three?/1)
+		(1..999) |> Enum.filter(&is_multiple_of_three_or_five?/1)
 	end
 
-	def sum_of_multiples_of_three_and_five_under(num) do
-		multiples_of_three_and_five_under(num) |> Enum.sum
+	def sum_of_multiples_of_three_or_five_under(num) do
+		multiples_of_three_or_five_under(num) |> Enum.sum
 	end
 	
 	
@@ -43,24 +46,30 @@ defmodule Problem1Test do
     	assert is_multiple_of?(2,14)
     	refute is_multiple_of?(2,15)
     end
-	test "#is_multiple_of_three_and_five" do
-		assert is_multiple_of_three_and_five?(15)
-		refute is_multiple_of_three_and_five?(9)
-		refute is_multiple_of_three_and_five?(20)
-		refute is_multiple_of_three_and_five?(4)
+	test "#is_multiple_of_three_or_five" do
+		assert is_multiple_of_three_or_five?(15)
+		assert is_multiple_of_three_or_five?(9)
+		assert is_multiple_of_three_or_five?(20)
+		refute is_multiple_of_three_or_five?(4)
 	end
 
-	test "#multiples_of_three_and_five_under 1000 should not contain non-multiples" do
-		refute multiples_of_three_and_five_under(1000) |> Enum.member?(9)
-		refute multiples_of_three_and_five_under(1000) |> Enum.member?(10)
+	test "#multiples_of_three_or_five_under 1000 should not contain non-multiples" do
+		assert multiples_of_three_or_five_under(1000) |> Enum.member?(9)
+		assert multiples_of_three_or_five_under(1000) |> Enum.member?(10)
 	end
 
-	test "#multiples_of_three_and_five_under 1000 should contain actual multiples" do
-		assert multiples_of_three_and_five_under(1000) |> Enum.member?(15)
-		assert multiples_of_three_and_five_under(1000) |> Enum.member?(30)
+	test "#multiples_of_three_or_five_under 1000 should contain actual multiples" do
+		assert multiples_of_three_or_five_under(1000) |> Enum.member?(15)
+		assert multiples_of_three_or_five_under(1000) |> Enum.member?(30)
 	end
 
-	test "sum_of_multiples_of_three_and_five_under 1000" do
-		assert sum_of_multiples_of_three_and_five_under(1000) == 33165
+	test "sum_of_multiples_of_three_or_five_under 1000" do
+		assert sum_of_multiples_of_three_or_five_under(1000) == 233168
 	end			
+
+	test "Problem1Test" do
+		result = sum_of_multiples_of_three_or_five_under(1000)
+		assert "e1edf9d1967ca96767dcc2b2d6df69f4" == :os.cmd('echo -n #{result} | md5sum') |> List.to_string |> String.split(" ") |> List.first
+		IO.puts "Anser is #{result}"
+	end
 end
